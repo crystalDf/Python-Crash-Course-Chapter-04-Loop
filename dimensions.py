c962fe3a354c0233f7d7b1a2417d4c193c9fea9f
crystalDf <chendong333@gmail.com>
2016-01-31 19:25:09 +0800

    Loop

---
+++ dimensions.py
@@ -0,0 +1,5 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+# dimensions[0] = 100
---
+++ foods.py
@@ -0,0 +1,35 @@
+my_foods = ["pizza", "falafel", "carrot cake"]
+friend_foods = my_foods[ : ]
+
+print("My favorite foods are: ")
+print(str(my_foods) + "\n")
+
+print("My friend's favorite foods are: ")
+print(str(friend_foods) + "\n")
+
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
+print("My favorite foods are: ")
+print(str(my_foods) + "\n")
+
+print("My friend's favorite foods are: ")
+print(str(friend_foods) + "\n")
+
+my_foods = ["pizza", "falafel", "carrot cake"]
+friend_foods = my_foods
+
+print("My favorite foods are: ")
+print(str(my_foods) + "\n")
+
+print("My friend's favorite foods are: ")
+print(str(friend_foods) + "\n")
+
+my_foods.append("cannoli")
+friend_foods.append("ice cream")
+
+print("My favorite foods are: ")
+print(str(my_foods) + "\n")
+
+print("My friend's favorite foods are: ")
+print(str(friend_foods) + "\n")
